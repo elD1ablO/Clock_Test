257019dd45b450888eea978abb0ecd853e5d8b24
Maksym <eldiablo@ukr.net>
2022-07-14 15:30:11 +0300

    functionality added

--- Assets/Scripts/Clock.cs
+++ Assets/Scripts/Clock.cs
@@ -7,10 +7,12 @@ using UnityEngine.UI;
 
 public class Clock : MonoBehaviour
 {
-    float secondsPerInGameDay = 6f;    //it's 86400f;
+    float secondsPerInGameDay = 6f;    //normally it's 86400f;
     
     float day;
     float analogModifier = 2f;
+
+    [SerializeField] Image background;
 
     [SerializeField] Transform hourClockHand;
     [SerializeField] Transform minuteClockHand;
@@ -19,13 +21,47 @@ public class Clock : MonoBehaviour
     [SerializeField] TextMeshProUGUI clockText;
     [SerializeField] TextMeshProUGUI daysText;
 
-    int daysToFinish = 30;
+    #region VALUES
+    float currentHour;
 
+    string hoursString;
+    string minutesString;
+    string secondsString;
+    #endregion
+
+    int daysToFinish = 30;
     int daysPassed = 1;
         
     void Update()
     {
         RotateClock();
+    }
+    private void FixedUpdate()
+    {
+        // days counting put into fixed update due to issues caused by update specifics
+        float dayNormalized = day % 1f;
+        float hoursPerDay = 24f;
+        
+        float hours = dayNormalized * hoursPerDay;
+        if (hours >= 23.99)
+        {
+            daysPassed++;
+        }
+        daysText.text = daysPassed.ToString();
+
+        //here changes colour of background
+        if (currentHour >= 11 && currentHour < 20)
+        {
+            background.color = Color.green;
+        }
+        else if (currentHour >= 20 || currentHour < 6)
+        {
+            background.color = Color.blue;
+        }
+        else if (currentHour <= 11 && currentHour >= 6)
+        {
+            background.color = Color.white;
+        }
     }
 
     void RotateClock()
@@ -46,20 +82,44 @@ public class Clock : MonoBehaviour
         float minutesPerHour = 60f;
         secondsClockHand.eulerAngles = new Vector3(0, 0, -dayNormalized * rotationPerDay * hoursPerDay * minutesPerHour);
 
-        float hours = dayNormalized * hoursPerDay;
 
-        string hoursString = Mathf.Floor(dayNormalized * hoursPerDay).ToString("00");
-        string minutesString = Mathf.Floor((dayNormalized * hoursPerDay) % 1f * minutesPerHour).ToString("00");
-        string secondsString = Mathf.Floor(((dayNormalized * hoursPerDay) * minutesPerHour) % 1f * minutesPerHour).ToString("00");
+        //these is values of 24h clock
+        currentHour = Mathf.Floor(dayNormalized * hoursPerDay);
+
+        hoursString = currentHour.ToString("00");
+        minutesString = Mathf.Floor((dayNormalized * hoursPerDay) % 1f * minutesPerHour).ToString("00");
+        secondsString = Mathf.Floor(((dayNormalized * hoursPerDay) * minutesPerHour) % 1f * minutesPerHour).ToString("00");       
         
+
+        //visualization of 24h clock
         clockText.text = hoursString + ":" + minutesString + ":" + secondsString;
 
 
-        if(hours >= 23.999)
+    }
+
+    public void PeriodOfDay()
+    {
+        if (currentHour >= 11 && currentHour < 20)
         {
-            daysPassed++;
+            Debug.Log("Day");
         }
-        daysText.text = daysPassed.ToString();
-        
+        else if(currentHour >= 20 || currentHour < 6)
+        {
+            Debug.Log("Night");
+        }
+        else if( currentHour <= 11 && currentHour >= 6)
+        {
+            Debug.Log("Morning");
+        }
+    }
+
+    public void CurrentTime()
+    {
+        Debug.Log(hoursString + ":" + minutesString + ":" + secondsString);
+    }
+
+    public void TimeSinceStart()
+    {
+        Debug.Log("Day: " + daysPassed + "  Current time is " + hoursString + ":" + minutesString + ":" + secondsString);
     }
 }
